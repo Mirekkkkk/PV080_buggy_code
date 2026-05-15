2fc0295aeb9c5ca8f47d2eb73d7694300d01e973
Mirekkkkk <miroslav.zolvik05@gmail.com>
2026-05-15 11:11:22 +0200

    fixed use of exec

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -23,11 +23,7 @@ def print_nametag(format_string, person):
     print(format_string.format(person=person))
 
 
-def fetch_website(urllib_version, url):
-    # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
-    # Fetch and print the requested URL
- 
+def fetch_website(url):
     try: 
         http = urllib.PoolManager()
         r = http.request('GET', url)
